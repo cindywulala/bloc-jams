e451616f64a36483bedae8108278382fec6a2b5e
Mengyi Chen <cindychenia@gmail.com>
2017-07-20 16:46:38 -0500

    Checkpoint 9 DOM Scripting Events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,37 @@
-var revealPoint = function() {
+window.onload = function() {
 
-      var points = document.getElementsByClassName("point");
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
 
-      var revealPoint = function(index) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-      }
+  var sellingPoints = document.getElementsByClassName("selling-points")[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-      for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-      }
+  window.addEventListener("scroll", function(event) {
+
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+
+  });
+
+};
+
+
+var pointsArray = document.getElementsByClassName("point");
+
+function revealPoint(point) {
+
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+
+}
+
+var animatePoints = function(points) {
+
+  forEach(points, revealPoint);
 
 };
 
